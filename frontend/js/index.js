92bfc47e780eeb117f16e8e350eae3f8d67a3b0a
Ahmet Emre Aladağ <aladagemre@gmail.com>
2016-05-04 13:53:04 +0300

    Popup closes itself

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,4 @@
-window.setToken = function (token, screen_name) {
+window.setToken = function(token, screen_name) {
   console.log('Setting token:', token)
   console.log('Setting screen_name:', screen_name)
   localStorage.setItem('token', token)
@@ -6,7 +6,7 @@ window.setToken = function (token, screen_name) {
   setCredentials(token, screen_name)
 }
 
-function setCredentials(token, screen_name){
+function setCredentials(token, screen_name) {
   var span_screen_name = document.getElementById('screen_name')
   var span_token = document.getElementById('token')
   var span_status = document.getElementById('status')
@@ -17,56 +17,50 @@ function setCredentials(token, screen_name){
   span_status.innerHTML = loggedin ? "Logged in" : "Not logged in"
 }
 
-function logout () {
+function logout() {
   setCredentials("", "")
 }
 
-window.onload = function () {
+window.onload = function() {
   var token = localStorage.getItem('token')
   var screen_name = localStorage.getItem('screen_name')
   console.log(token)
   setCredentials(token, screen_name)
 }
 
-function login () {
+function login() {
   var defaults = {
     name: 'twitter',
     url: '/auth/twitter',
     authorizationEndpoint: 'https://api.twitter.com/oauth/authenticate',
     redirectUri: window.location.origin,
     type: '1.0',
-    popupOptions: { width: 495, height: 645 }
+    popupOptions: {
+      width: 495,
+      height: 645
+    }
   }
-  var myWindow = window.open('_blank', 'twitter login', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=495,width=645');
+  var myWindow = window.open(window.location.origin + '/wait', 'twitter login',
+    'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=495,width=645'
+  );
   fetch('/auth/twitter', {
-  	method: 'POST',
-  	mode: 'cors',
-  	redirect: 'follow',
+    method: 'POST',
+    mode: 'cors',
+    redirect: 'follow',
     body: JSON.stringify(defaults),
     data: JSON.stringify(defaults),
     json: JSON.stringify(defaults),
-  	headers: new Headers({
-  		'Content-Type': 'application/json'
-  	})
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
   }).then(function(response) {
-      return response.json()
-   }).then(function(j){
-     data = j
-     var qs = 'oauth_callback_confirmed=' + data['oauth_callback_confirmed'] + '&oauth_token=' + data['oauth_token'] + '&oauth_token_secret=' + data['oauth_token_secret']
+    return response.json()
+  }).then(function(j) {
+    data = j
+    var qs = 'oauth_callback_confirmed=' + data['oauth_callback_confirmed'] +
+      '&oauth_token=' + data['oauth_token'] + '&oauth_token_secret=' + data[
+        'oauth_token_secret']
     myWindow.location.href = defaults.authorizationEndpoint + '?' + qs
-    var timer = setInterval(checkURL, 1000)
-    function checkURL () {
-      // TODO: what if we check before document load?
-      try {
-        var url = myWindow.location.href
-        if (url.indexOf(defaults.redirectUri) != -1){
-          console.log('Auth complete!')
-          clearInterval(timer);
-          myWindow.close()
-        }
-      }
-      catch(e) {}
-    }
     console.log(j)
   })
 }
